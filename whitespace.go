46a2df42a35ce691b96650aea51105a46d093afe
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-03-02 20:48:26 -0800

    Replace regular expression with individual rune inspection

--- whitespace.go
+++ whitespace.go
@@ -1,16 +1,28 @@
 package whitespace
 
-import "regexp"
+import (
+	"unicode"
+)
 
 // Remove returns the provided string with all of the whitespace removed.
-// This includes spaces, tabs, newlines, returns, and form feeds.
+// This includes spaces, tabs, newlines, returns, form feeds and other
+// space-like characters.
+//
+// For more information on what is considered whitespace, visit:
+// https://golang.org/pkg/unicode/#IsSpace
 func Remove(str string) string {
-	space := regexp.MustCompile(`\s+`)
-	return space.ReplaceAllString(str, "")
+	var out []rune
+	for _, r := range str {
+		if !unicode.IsSpace(r) {
+			out = append(out, r)
+		}
+	}
+
+	return string(out)
 }
 
-// IsBlank returns true if the provided string is empty or only consists of whitespace.
-// Returns false otherwise.
+// IsBlank returns true if the provided string is empty or consists only of
+// whitespace. Returns false otherwise.
 func IsBlank(str string) bool {
 	if Remove(str) == "" {
 		return true
@@ -20,7 +32,7 @@ func IsBlank(str string) bool {
 }
 
 // HasBlank returns true if any of the strings in the provided slice is empty
-// or consists of whitespace. If the slice is nil, returns true as well.
+// or consists of only whitespace. If the slice is nil, returns true as well.
 // Returns false otherwise.
 func HasBlank(slice []string) bool {
 	if len(slice) <= 0 {
@@ -32,5 +44,6 @@ func HasBlank(slice []string) bool {
 			return true
 		}
 	}
+
 	return false
 }
